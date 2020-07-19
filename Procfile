4d41a318712b6457f9bd5bf9c0b3d8036371508b
jqbergantinho97 <jqbergantinho@gmail.com>
2020-07-19 19:46:25 +0200

    procfile change

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn jeweldatabase.wsgi --log-file -
+web: gunicorn jewels.wsgi --log-file -
--- jeweldatabase/jeweldatabase/wsgi.py
+++ jeweldatabase/jeweldatabase/wsgi.py
@@ -7,9 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jeweldatabase.settings")
 
